28449a39c4f08829ff68802bd24fb383bccd353b
Lucas <lucas.buzzoferreira@hotmail.com>
2022-06-13 03:29:17 -0300

    todoApp RecyclerView

---
+++ exandroidstudio/todoApp/app/src/main/java/com/generation/todoapp/model/Tarefa.kt
@@ -0,0 +1,11 @@
+package com.generation.todoapp.model
+
+data class Tarefa (
+    var nome: String,
+    var descricao: String,
+    var responsavel: String,
+    var data: String,
+    var status: Boolean,
+    var categoria: String
+        ) {
+}
